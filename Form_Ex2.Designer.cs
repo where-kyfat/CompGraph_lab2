f398cd2075c98ff3b9c0a7a0b04c19a6260e9b34
Nekdil01 <36044753+Nekdil01@users.noreply.github.com>
2019-09-19 22:27:58 +0300

    Revert "Revert "Revert "Merge branch 'master' into Form_Ex1"""
    This reverts commit 7cde598f8d899ea589ae3feb67118e8f31d307ad.

--- Form_Ex2.Designer.cs
+++ Form_Ex2.Designer.cs
@@ -28,88 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox_Image = new System.Windows.Forms.PictureBox();
-            this.pictureBox_Gist = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Gist)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox_Image
-            // 
-            this.pictureBox_Image.Image = global::ImageGrayWF.Properties.Resources.ФРУКТЫ;
-            this.pictureBox_Image.Location = new System.Drawing.Point(42, 12);
-            this.pictureBox_Image.Name = "pictureBox_Image";
-            this.pictureBox_Image.Size = new System.Drawing.Size(256, 414);
-            this.pictureBox_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox_Image.TabIndex = 0;
-            this.pictureBox_Image.TabStop = false;
-            // 
-            // pictureBox_Gist
-            // 
-            this.pictureBox_Gist.Location = new System.Drawing.Point(355, 12);
-            this.pictureBox_Gist.Name = "pictureBox_Gist";
-            this.pictureBox_Gist.Size = new System.Drawing.Size(256, 414);
-            this.pictureBox_Gist.TabIndex = 0;
-            this.pictureBox_Gist.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(671, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 48);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "To R";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(671, 182);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 48);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "To G";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(671, 260);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 48);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "To B";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form_Ex2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox_Gist);
-            this.Controls.Add(this.pictureBox_Image);
             this.Name = "Form_Ex2";
             this.Text = "Form_Ex2";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Ex2_FormClosing);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Gist)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox_Image;
-        private System.Windows.Forms.PictureBox pictureBox_Gist;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
