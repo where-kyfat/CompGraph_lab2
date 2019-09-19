860237f85b02acb77cec8e28c8a85909b3214aed
Oleg Shabarshin <oleg_shabarshin@mail.ru>
2019-09-20 00:34:16 +0300

    Реализована задача 3

--- Form_Ex3.Designer.cs
+++ Form_Ex3.Designer.cs
@@ -28,20 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.HueTrackBar = new System.Windows.Forms.TrackBar();
+            this.SaturationTrackBar = new System.Windows.Forms.TrackBar();
+            this.BrightnessTrackBar = new System.Windows.Forms.TrackBar();
+            this.ImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.RGBtoHSVButton = new System.Windows.Forms.Button();
+            this.SaveImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.HueTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrightnessTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // HueTrackBar
+            // 
+            this.HueTrackBar.Location = new System.Drawing.Point(600, 100);
+            this.HueTrackBar.Maximum = 360;
+            this.HueTrackBar.Name = "HueTrackBar";
+            this.HueTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.HueTrackBar.TabIndex = 0;
+            // 
+            // SaturationTrackBar
+            // 
+            this.SaturationTrackBar.Location = new System.Drawing.Point(600, 167);
+            this.SaturationTrackBar.Maximum = 1000;
+            this.SaturationTrackBar.Name = "SaturationTrackBar";
+            this.SaturationTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.SaturationTrackBar.TabIndex = 1;
+            // 
+            // BrightnessTrackBar
+            // 
+            this.BrightnessTrackBar.Location = new System.Drawing.Point(600, 237);
+            this.BrightnessTrackBar.Maximum = 100;
+            this.BrightnessTrackBar.Name = "BrightnessTrackBar";
+            this.BrightnessTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.BrightnessTrackBar.TabIndex = 2;
+            // 
+            // ImagePictureBox
+            // 
+            this.ImagePictureBox.Image = global::ImageGrayWF.Properties.Resources.ФРУКТЫ;
+            this.ImagePictureBox.Location = new System.Drawing.Point(54, 56);
+            this.ImagePictureBox.Name = "ImagePictureBox";
+            this.ImagePictureBox.Size = new System.Drawing.Size(401, 274);
+            this.ImagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImagePictureBox.TabIndex = 3;
+            this.ImagePictureBox.TabStop = false;
+            // 
+            // RGBtoHSVButton
+            // 
+            this.RGBtoHSVButton.Location = new System.Drawing.Point(610, 307);
+            this.RGBtoHSVButton.Name = "RGBtoHSVButton";
+            this.RGBtoHSVButton.Size = new System.Drawing.Size(75, 23);
+            this.RGBtoHSVButton.TabIndex = 4;
+            this.RGBtoHSVButton.Text = "button1";
+            this.RGBtoHSVButton.UseVisualStyleBackColor = true;
+            this.RGBtoHSVButton.Click += new System.EventHandler(this.RGBtoHSVbutton_Click_Click);
+            // 
+            // SaveImageButton
+            // 
+            this.SaveImageButton.Location = new System.Drawing.Point(212, 358);
+            this.SaveImageButton.Name = "SaveImageButton";
+            this.SaveImageButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveImageButton.TabIndex = 5;
+            this.SaveImageButton.Text = "button1";
+            this.SaveImageButton.UseVisualStyleBackColor = true;
+            this.SaveImageButton.Click += new System.EventHandler(this.SaveImageButton_Click);
             // 
             // Form_Ex3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveImageButton);
+            this.Controls.Add(this.RGBtoHSVButton);
+            this.Controls.Add(this.ImagePictureBox);
+            this.Controls.Add(this.BrightnessTrackBar);
+            this.Controls.Add(this.SaturationTrackBar);
+            this.Controls.Add(this.HueTrackBar);
             this.Name = "Form_Ex3";
-            this.Text = "Form_Ex3";
+            this.Text = "Ex3_RGB->HSV";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Ex3_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.HueTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SaturationTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrightnessTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar HueTrackBar;
+        private System.Windows.Forms.TrackBar SaturationTrackBar;
+        private System.Windows.Forms.TrackBar BrightnessTrackBar;
+        private System.Windows.Forms.PictureBox ImagePictureBox;
+        private System.Windows.Forms.Button RGBtoHSVButton;
+        private System.Windows.Forms.Button SaveImageButton;
     }
 }
--- Form_Ex3.cs
+++ Form_Ex3.cs
@@ -13,15 +13,106 @@ namespace ImageGrayWF
     public partial class Form_Ex3 : Form
     {
         private Form_Main FormMain;
+        private Image OriginImage;
+
         public Form_Ex3(Form_Main form_Main)
         {
             InitializeComponent();
             FormMain = form_Main;
+            OriginImage = ImagePictureBox.Image;
         }
 
         private void Form_Ex3_FormClosing(object sender, FormClosingEventArgs e)
         {
             FormMain.Show();
+        }
+
+        private void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
+        }
+
+        private Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+
+
+
+        private void RGBtoHSVbutton_Click_Click(object sender, EventArgs e)
+        {
+            ImagePictureBox.Image = OriginImage;
+            double hue;
+            double saturation;
+            double value;
+            Bitmap RGBtoHSV_Image = new Bitmap(OriginImage);
+
+
+            for (int x = 0; x < RGBtoHSV_Image.Width; x++)
+            {
+                for (int y = 0; y < RGBtoHSV_Image.Height; y++)
+                {
+                    Color color = RGBtoHSV_Image.GetPixel(x, y);
+                    ColorToHSV(color, out hue, out saturation, out value);
+                    hue += HueTrackBar.Value % 360;
+                    saturation += SaturationTrackBar.Value / 1000.0;
+                    saturation = saturation > 1 ? saturation - 1: saturation;
+                    value += BrightnessTrackBar.Value / 100.0;
+                    value = value > 1 ? value - 1 : value;
+                    Color color1 = ColorFromHSV(hue, saturation, value);
+                    RGBtoHSV_Image.SetPixel(x, y, Color.FromArgb(color1.R, color1.G, color1.B));
+                }
+            }
+            ImagePictureBox.Image = RGBtoHSV_Image;
+            Image image = RGBtoHSV_Image;
+        }
+
+        private void SaveImageButton_Click(object sender, EventArgs e)
+        {
+            //создание диалогового окна "Сохранить как..", для сохранения изображения
+            SaveFileDialog savedialog = new SaveFileDialog();
+            savedialog.Title = "Сохранить картинку как...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+            savedialog.ShowHelp = true;
+            if (savedialog.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+            {
+                try
+                {
+                    ImagePictureBox.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
